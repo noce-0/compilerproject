0943259118f68cbb50b30882660ea7e52b299a8e
noce-0 <65069951+noce-0@users.noreply.github.com>
2024-10-01 16:45:54 +0200

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -1,5 +1,7 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
+#include <string>
+#include <vector>
 
 /---------- AST DIDDY ----------/
 class Node {
@@ -103,7 +105,7 @@ public:
     Block();
     ~Block() = default;
 
-    int interpret() override;
+    int interpret() override;   // perché foglia
 
 private:
     IfStmt* ifblock;
@@ -112,6 +114,45 @@ private:
     DoStmt* doblock;
     PrintStmt* printblock;
     BreakStmt* breakblock;
+};
+
+class OpExpr : public Expression {
+    public:
+        OpExpr();
+        ~OpExpr() override = default;
+        virtual int interpret() = 0;    // perché padre
+};
+
+class UnaryOp final : public OpExpr {
+    public:
+        UnaryOp(const char c, int o) : ch(c), op(o) {};
+        ~UnaryOp() override = default;
+        int interpret() override;
+    private:
+        const char ch = '-';
+        int op;
+};
+
+class AccessOp final : public OpExpr {
+    public:
+        AccessOp(int ind, char o, char c) : index(ind), op(o), cb(c) {};
+        ~AccessOp() override = default;
+        int interpret() override;
+    private:
+        int index;
+        const char op = '[';
+        const char cb = ']';
+};
+
+class BinaryOp final : public OpExpr {
+    public:
+        BinaryOp(int l, int r, char o) : left_op(l), right_op(o), op(o) {};
+        ~BinaryOp() override = default;
+        int interpret() override;
+    private:
+        int left_op;
+        int right_op;
+        char op;
 };
 
 #endif //INTERPRETER_H
